999ac8588819a98504e24f38e3fb689a1149d6d1
josemorista <josemorista@id.uff.br>
2019-10-15 15:25:30 -0300

    initial commit for real-time

--- js/script.js
+++ js/script.js
@@ -1,40 +1,19 @@
 document.body.classList.add("loading");
 
+const FPS = 30;
+
+let src
+let dst
+let cap
 let initialObj
 let finalObj
 
-document.getElementById('hsvSliders').addEventListener('change', (e) => {
-  initialObj.setHSVFilter([parseFloat(document.getElementById('initialLowH').value), parseFloat(document.getElementById('initialLowS').value), parseFloat(document.getElementById('initialLowV').value), 0], [parseFloat(document.getElementById('initialHighH').value), parseFloat(document.getElementById('initialHighS').value),parseFloat(document.getElementById('initialHighV').value), 255]);
-  finalObj.setHSVFilter([parseFloat(document.getElementById('finalLowH').value), parseFloat(document.getElementById('finalLowS').value), parseFloat(document.getElementById('finalLowV').value), 0], [parseFloat(document.getElementById('finalHighH').value), parseFloat(document.getElementById('finalHighS').value), parseFloat(document.getElementById('finalHighV').value), 255]);
-  processImage()
-})
-
-document.getElementById('realR').addEventListener('change', (e) => {
-  initialObj.setRealR(e.target.value)
-  finalObj.setRealR(e.target.value)
-  processImage()
-})
-
-let imgElement = document.getElementById('imageOriginal');
-let inputElement = document.getElementById('imageInput');
-inputElement.addEventListener('change', (e) => {
-  imgElement.src = URL.createObjectURL(e.target.files[0]);
-}, false);
-
-imgElement.onload = function() {
-  let src
-  src = cv.imread(imgElement);
-  cv.imshow('imageCanvas', src);
-  src.delete()
-  processImage()
-};
-
-function processImage() {
+function processVideo() {
   try {
+    let begin = Date.now();
+    
     // start processing.
-    this.disabled = true;
-    let src = cv.imread(imgElement);
-    let dst = src.clone()
+    cap.read(src);
     
     cv.cvtColor(src, dst, cv.COLOR_BGR2HSV, 0)
     
@@ -106,7 +85,7 @@ function processImage() {
     const distance = initialObj.calculateDistanceBetweenObjct(finalObj)
 
     if (distance)
-      document.getElementById('results').innerHTML =`The distance is ${distance.distanceBetweenCm.toFixed(8)} cm!`
+      document.getElementById('results').innerHTML =`The distance is ${distance.distanceBetweenCm.toFixed(2)} cm!`
 
     cv.imshow('imageCanvas', src)
 
@@ -116,23 +95,72 @@ function processImage() {
     highFinal.delete()
     iThresholded.delete()
     fThresholded.delete()
-    src.delete()
-    dst.delete();
+    
+    // schedule the next one.
+    let delay = 1000 / FPS - (Date.now() - begin);
+    setTimeout(processVideo, delay);
   } catch (err) {
     console.log(err)
   }
 };
 
+
+document.getElementById('hsvSliders').addEventListener('change', (e) => {
+  initialObj.setHSVFilter([parseFloat(document.getElementById('initialLowH').value), parseFloat(document.getElementById('initialLowS').value), parseFloat(document.getElementById('initialLowV').value), 0], [parseFloat(document.getElementById('initialHighH').value), parseFloat(document.getElementById('initialHighS').value),parseFloat(document.getElementById('initialHighV').value), 255]);
+  finalObj.setHSVFilter([parseFloat(document.getElementById('finalLowH').value), parseFloat(document.getElementById('finalLowS').value), parseFloat(document.getElementById('finalLowV').value), 0], [parseFloat(document.getElementById('finalHighH').value), parseFloat(document.getElementById('finalHighS').value), parseFloat(document.getElementById('finalHighV').value), 255]);
+})
+
+document.getElementById('realR').addEventListener('change', (e) => {
+  initialObj.setRealR(e.target.value)
+  finalObj.setRealR(e.target.value)
+})
+
+window.onresize = () => {
+  let video = document.getElementById('videoInput')
+  src.delete()
+  dst.delete()
+  let videoContainer = document.getElementById('videoContainer')
+  video.width = parseInt(videoContainer.clientWidth) <= 800 ? videoContainer.clientWidth : '800'
+  video.height = `${parseInt(video.width) * 0.75}`
+  src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+  dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+}
+
 function onOpenCvReady() {
+  let video = document.getElementById('videoInput')
+  let videoContainer = document.getElementById('videoContainer')
+  video.width = parseInt(videoContainer.clientWidth) <= 800 ? videoContainer.clientWidth : '800'
+  video.height = `${parseInt(video.width) * 0.75}`
+
+  video.addEventListener('play', () => {
+    let video = document.getElementById('videoInput')
+    src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    cap = new cv.VideoCapture(video);
+    setTimeout(processVideo, 0);
+  })
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(function (stream) {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(function (err) {
+      console.log("An error occurred! " + err);
+    });
   
-  let realR = document.getElementById('realR').value
+    let realR = document.getElementById('realR').value
 
   initialObj = new TrackingObject(cv, realR, 1000)
   finalObj = new TrackingObject(cv, realR, 1000)
   
   initialObj.setHSVFilter([parseFloat(document.getElementById('initialLowH').value), parseFloat(document.getElementById('initialLowS').value), parseFloat(document.getElementById('initialLowV').value), 0], [parseFloat(document.getElementById('initialHighH').value), parseFloat(document.getElementById('initialHighS').value),parseFloat(document.getElementById('initialHighV').value), 255]);
   finalObj.setHSVFilter([parseFloat(document.getElementById('finalLowH').value), parseFloat(document.getElementById('finalLowS').value), parseFloat(document.getElementById('finalLowV').value), 0], [parseFloat(document.getElementById('finalHighH').value), parseFloat(document.getElementById('finalHighS').value), parseFloat(document.getElementById('finalHighV').value), 255]);
-  
+
   document.body.classList.remove("loading");
   document.body.classList.remove("hidden");
+}
+
+window.onclose = () => {
+  src.delete()
+  dst.delete()
 }
